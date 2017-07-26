70bd5be37afeec461f73e9e91c7462552820f2a7
Ryosuke Honda <honda00888005@gmail.com>
2017-07-26 13:30:58 +0900

    implemented nearest_approaching car

--- src/helper.cpp
+++ src/helper.cpp
@@ -8,6 +8,13 @@ using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using std::vector;
 
+
+struct Trajectory{
+    vector<double> s_coeffs;
+    vector<double> d_coeffs;
+    double t;
+};
+
 Vehicle::Vehicle(int id, double x, double y, double vx, double vy, double s, double d, double a, double d_dot, double d_ddot){
 
   this->id = id;
@@ -63,3 +70,45 @@ vector<double> differentiate(vector<double> coefficients){
     }
     return new_cos;
 }
+
+double nearest_approach(Trajectory traj, Vehicle vehicle){
+    double closest = 999999;
+    vector<double> s_;
+    vector<double> d_;
+    double T;
+    s_ = traj.s_coeffs;
+    d_ = traj.d_coeffs;
+    T = traj.t;
+    double cur_s, cur_d;
+    double targ_s,targ_d;
+    double dist;
+    double t;
+    for(int i=0;i<100;i++){
+        t = float(i)/100 * T;
+        cur_s = f_val(t,s_);
+        cur_d = f_val(t,d_);
+
+        targ_s = vehicle.state_at(t)[0];
+        targ_d = vehicle.state_at(t)[3];
+        //cout<<"Targ_s: "<<targ_s<<" Targ_d: "<<targ_d<<endl;
+        dist = sqrt(pow((cur_s-targ_s),2)+pow(cur_d-targ_d,2));
+        if(dist<closest){
+            closest = dist;
+        }
+    }
+    return closest;
+}
+
+double nearest_approach_to_any_vehicle(Trajectory traj, vector<Vehicle> vehicles){
+    double closest =999999;
+    double d;
+    for(int i=0; i<vehicles.size();i++){
+        d = nearest_approach(traj,vehicles[i]);
+        cout<<i<<"   "<<d<<endl;
+        if(d<closest){
+            closest = d;
+        }
+    }
+    return closest;
+}
+
